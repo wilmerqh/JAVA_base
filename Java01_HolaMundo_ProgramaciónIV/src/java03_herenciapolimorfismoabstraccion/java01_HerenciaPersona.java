21433309276b96c2342d8a1180b639cefb50aa36
Wilmer Quim <wilmerabrahamqh@gmail.com>
2026-09-01 15:55:38 -0600

    agregar comentario

--- Java01_HolaMundo_ProgramaciónIV/src/java03_herenciapolimorfismoabstraccion/java01_HerenciaPersona.java
+++ Java01_HolaMundo_ProgramaciónIV/src/java03_herenciapolimorfismoabstraccion/java01_HerenciaPersona.java
@@ -8,6 +8,18 @@ package java03_herenciapolimorfismoabstraccion;
  *
  * @author WilChess
  */
+/*
+La herencia permite crear una clase nueva a partir de otra ya
+existente, aprovechando sus atributos y métodos. La clase de
+la que se hereda se llama superclase (o clase padre) y la que
+hereda se llama subclase (o clase hija). Para indicar herencia
+se usa la palabra extends. Imagina que en un sistema escolar
+tenemos personas: estudiantes y profesores. Todos comparten
+datos comunes (nombre, edad), pero cada uno tiene datos
+propios. En lugar de repetir nombre y edad en cada clase,
+los ponemos en una superclase Persona y dejamos que las
+demás hereden de ella.
+*/
 public class java01_HerenciaPersona {
     
 }
